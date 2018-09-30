4c7b1b7a9a24ec321357ef75e86628d3c1666972
irwin.jiangwei <17735528@qq.com>
2018-09-30 16:28:54 +0800

    add memory layout example

--- memory layout of c programs/mem layout.c
+++ memory layout of c programs/mem layout.c
@@ -1,11 +1,19 @@
+/*
+compile and generate .o file and use size command to checkout the mem layout
 
+$gcc "mem layout.c" -o memlayout.o
+ 
 
+$ size memlayout.o
+   text    data     bss     dec     hex filename
+  14428    1552     124   16104    3ee8 memlayout.o
+
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int ga_i = 0;   // global initialized segment
-
 
 char *p1;     // global uninitialized segment
 
@@ -17,7 +25,7 @@ int main(int argc, char const *argv[])
 
     char *p2;           //stack
 
-    static int a = 0;   //golbal initialized segment
+    static int a = 0;   //static initialized segment
 
     p2 = malloc(100);     
     printf("&p2: %p\n", &p2);   // p2 as the pointer variable, it address is in the stack;
